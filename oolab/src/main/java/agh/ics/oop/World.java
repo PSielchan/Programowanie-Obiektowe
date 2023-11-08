72d9132e78c4ac987ef64073b597b2b18340da24
PSielchan <147639332+PSielchan@users.noreply.github.com>
2023-11-08 12:31:35 +0100

    program napisany

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,31 +1,20 @@
 package agh.ics.oop;
 
-import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.MapDirection;
+import agh.ics.oop.model.Vector2d;
 
 public class World {
-    static void run(MoveDirection[] napisy){
-        for (int i = 0; i < napisy.length; i++) {
-            switch (napisy[i]){
-                case FORWARD:
-                    System.out.println("Zwierzak idzie do przodu");
-                    break;
-                case BACKWARD:
-                    System.out.println("Zwierzak idzie do tyłu");
-                    break;
-                case RIGHT:
-                    System.out.println("Zwierzak skręca w prawo");
-                    break;
-                case LEFT:
-                    System.out.println("Zwierzak skręca w lewo");
-                    break;
-                default:
-            }
-        }
 
-    }
     public static void main(String[] args) {
-        System.out.println("Start");
-        run(OptionsParser.main(args));
-        System.out.println("Stop");
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+
+        System.out.println(MapDirection.WEST.toString());
+        System.out.println(MapDirection.WEST.next());
+        System.out.println(MapDirection.WEST.previous());
+        System.out.println(MapDirection.WEST.toUnitVector());
     }
 }
---
+++ oolab/src/test/java/OptionsParserTest.java
@@ -0,0 +1,9 @@
+import agh.ics.oop.OptionsParser;
+import agh.ics.oop.model.MoveDirection;
+
+public class OptionsParserTest {
+    public void MoveDirection(){
+        String[] Tab = new String[4];
+        Tab[0]="f";Tab[1]="f";Tab[2]="r";Tab[3]="l";
+    }
+}
